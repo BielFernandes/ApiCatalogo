2cace24399fafdf14e37d0457aa1143ec3bd3336
BielFernandes <gabrielfdias082@gmail.com>
2024-05-07 10:29:07 -0300

    contexto e migrações / data anottations

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,9 +1,27 @@
-﻿namespace ApiCatalogo.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImageUrl { get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; }
     }
 }
