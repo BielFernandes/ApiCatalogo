794bab969ab6bfad7490c2ede3fe4dba91fe5729
BielFernandes <gabrielfdias082@gmail.com>
2024-06-11 11:33:48 -0300

    aplicando unit of work

---
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿
+using APICatalogo.Context;
+
+namespace APICatalogo.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IProdutoRepository? _produtoRepo;
+        public ICategoriaRepository? _categoriaRepo;
+
+        public AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProdutoRepository ProdutoRepository
+        {
+
+           get
+            {
+                //if(_produtoRepo == null)
+                //{
+                //    _produtoRepo = new ProdutoRepository(_context);
+                //}
+                //return _produtoRepo;
+                return _produtoRepo ??= new ProdutoRepository(_context);
+            }
+        }
+
+        public ICategoriaRepository CategoriaRepository
+        {
+
+            get
+            {
+                return _categoriaRepo ??= new CategoriaRepository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
